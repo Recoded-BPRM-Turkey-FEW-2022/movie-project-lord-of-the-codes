315d6e85b19f2957cfa5be5061c5e467e0daadcd
beyzabostanci <beyza_bostanc@hotmail.com>
2022-05-26 00:32:20 +0300

    not done with search

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ const constructUrl = (path) => {
     "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
   )}`;
 };
-
 // You may need to add to this function, definitely don't delete it.
 const movieDetails = async (movie) => {
   const movieRes = await fetchMovie(movie.id);
@@ -30,6 +29,7 @@ const fetchMovies = async () => {
   const res = await fetch(url);
   return res.json();
 };
+
 
 // Don't touch this function please. This function is to fetch one movie.
 const fetchMovie = async (movieId) => {
@@ -50,6 +50,7 @@ const renderMovies = (movies) => {
   "flex-wrap","justify-content-around"
   );
 console.log(divMovies)
+
 /////////////
   movies.map((movie) => {
     const CardDiv = document.createElement("div");
@@ -67,7 +68,7 @@ console.log(divMovies)
     });
     CardDiv.appendChild(movieImage);
     CardDiv.appendChild(movieTitle);
-    divMovies.appendChild(CardDiv)
+    divMovies.appendChild(CardDiv);
     CONTAINER.appendChild(divMovies);
   });
 };
@@ -93,9 +94,70 @@ const renderMovie = (movie) => {
         </div>
         </div>
             <h3>Actors:</h3>
-            <ul id="actors" class="list-unstyled"></ul>
+
+          <ul id="actors" class="list-unstyled"></ul>
     </div>
   </div>`;
 };
 
 document.addEventListener("DOMContentLoaded", autorun);
+
+
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=23381f9d606c6cff00c6543b3446c4ad&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=23381f9d606c6cff00c6543b3446c4ad&query=" '
+const main = document.getElementById("content");
+const search = document.getElementById("search");
+
+
+const searchconstructUrl = (searchValue) => {
+  return `${TMDB_BASE_URL}/search/multi?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}&query=${searchValue}`;
+};
+
+const searchRes = async (searchValue) => {
+  const url = searchconstructUrl(searchValue);
+  const res = await fetch(url);
+  const json = await res.json();
+  return json;
+}
+
+
+const renderSearch = async (data) => {
+  const resDat = await searchRes(data)
+  const searchContent = document.getElementById("content");
+
+    if (resDat === undefined) {
+    alert("undefined")
+  } else if (resDat === movie){
+    searchContent.innerHTML = "";
+    resDat.results.forEach(element => {
+      const elementContainer = document.createElement("div")
+      elementContainer.innerHTML=`
+      <h3 class="red-text">${element.title}</h3>
+<img width="300" src="${IMG_PATH + element.backdrop_path}">
+`
+elementContainer.classList.add('className')
+  searchContent.appendChild(elementContainer)
+})
+  }else if(resDat === person){
+    searchContent.innerHTML="";
+    resDat.results.forEach(popi => {
+      const elementContainer = document.createElement("div")
+      elementContainer.innerHTML= `
+      <h3 class="red-text">${popi.name}</h3>
+      <img width ="300" src="${PROFILE_BASE_URL + popi.profile_path}">
+      `
+      searchContent.appendChild(elementContainer)
+    })
+  }
+}
+
+
+const form = document.getElementById("searchBtn");
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const results = await searchRes();
+  const searchIn = document.querySelector("#search");
+  searchIn.innerHTML = "";
+  renderSearch(results);
+});
