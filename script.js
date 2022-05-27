417deec07c4083485da3d298a056a5260b666257
beyzabostanci <beyza_bostanc@hotmail.com>
2022-05-27 10:32:55 +0300

    design home page

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const constructUrl = (path) => {
     "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
   )}`;
 };
+
 // You may need to add to this function, definitely don't delete it.
 const movieDetails = async (movie) => {
   const movieRes = await fetchMovie(movie.id);
@@ -73,9 +74,9 @@ const renderMovies = (movies) => {
     movieImage.classList.add("card-img-top");
     movieTitle.classList.add("card-title");
     CardDiv.innerHTML = `
-        <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${movie.title
+        <img class="image" src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${movie.title
 } poster">
-        <h4>${movie.title}</h4>`;
+        <h4 class="contentCard" >${movie.title}</h4>`;
     CardDiv.addEventListener("click", () => {
       movieDetails(movie);
     });
@@ -89,7 +90,7 @@ const renderMovies = (movies) => {
 const renderMovie = (movie) => {
   const firstsection=document.querySelector(".FirstSection");
   firstsection.setAttribute("style", "background-image: url(" +BACKDROP_BASE_URL + movie.backdrop_path+ ");background-size: cover;");
-  CONTAINER.setAttribute("style", "background-color: white;");  
+  CONTAINER.setAttribute("style", "background-color: white;");
   CONTAINER.innerHTML = `
   <div class="movDetail">
     <div class="row">
@@ -112,19 +113,19 @@ let aboutUsNav = document.getElementById('about-us');
 aboutUsNav.addEventListener("click", async function () {
   AboutUsPage.renderAboutUs()
 });
-/////// About Us page rendering  
+/////// About Us page rendering
 class AboutUsPage {
-  static container = document.querySelector(".FirstSection");
+  static container = document.querySelector(".FirstSection");//dikkat et buraya, sadece bu sayfayı etkileyecek css lazım
   static renderAboutUs() {
     AboutUsPage.container.innerHTML = "";
     const AboutUsDiv = document.createElement("div");
-    AboutUsDiv.classList.add("card");
-    AboutUsDiv.classList.add("mb-3");
+    AboutUsDiv.classList.add("cardAct");
+    //AboutUsDiv.classList.add("mb-3");
     this.container.appendChild(AboutUsDiv)
 
     const abousUsImg = document.createElement("img");
-    abousUsImg.classList.add("card-img-top");
-    abousUsImg.src = "1.webp";
+    abousUsImg.classList.add("aboutUsImage");
+    abousUsImg.src = "1.webp";//image ayarla
     AboutUsDiv.appendChild(abousUsImg)
 
     const AboutUsDivBody = document.createElement("div");
@@ -132,15 +133,20 @@ class AboutUsPage {
     AboutUsDiv.appendChild(AboutUsDivBody)
 
     const AboutUsDivTitle = document.createElement("h5");
-    AboutUsDivTitle.classList.add("card-title");
+    AboutUsDivTitle.classList.add("aboutUs");
     AboutUsDivTitle.textContent = "Let's talk about Movie Hub";
     AboutUsDivBody.appendChild(AboutUsDivTitle)
-    
 
-    const AboutUsDivTxt = document.createElement("P");
-    AboutUsDivTxt.classList.add("card-text");
-    AboutUsDivTxt.innerHTML = "The Movie Hub is a website built out of TMDb database. Our main goal is to bring you all the Movie information you need. New movies, Popular movies or Old movies; you'll find it all here. <br><br><strong> Enjoy browsing Movie Hub</strong>";
+
+    const AboutUsDivTxt = document.createElement("p");
+    AboutUsDivTxt.classList.add("aboutUs");
+    AboutUsDivTxt.innerHTML = "<br><br><br> <br>The Movie Hub is a website built out of TMDb database. Our main goal is to bring you all the Movie information you need. New movies, Popular movies or Old movies; you'll find it all here.";
     AboutUsDivBody.appendChild(AboutUsDivTxt)
+
+    const AboutUsDivNote = document.createElement("h4");
+    AboutUsDivNote.classList.add("aboutUs");
+    AboutUsDivNote.innerHTML = "<br><br><br><br><br> Enjoy browsing Movie Hub";
+    AboutUsDivTxt.appendChild(AboutUsDivNote)
   }
 }
 
@@ -176,20 +182,20 @@ const renderPopularMovies = (movies) => {
     CardDiv.addEventListener("click", () => {
       movieDetails(movie);
     });
-  
+
     CardDiv.appendChild(movieImage);
     CardDiv.appendChild(movieTitle);
     divMovies.appendChild(CardDiv)
     CONTAINER.appendChild(divMovies);
   });
-  
+
 };
 let popularNav = document.getElementById('popular');
 popularNav.addEventListener("click", async function () {
   const popular = await fetchPopularMovies();
   const CONTAINER = document.querySelector(".container-fluid");
   CONTAINER.innerHTML = "";
-  renderPopularMovies(popular);  
+  renderPopularMovies(popular);
 });
 
 const fetchReleasedate= async()=> {
@@ -204,7 +210,7 @@ releasedate.addEventListener("click", async function () {
   const releaseDateRender = await fetchReleasedate();
   const CONTAINER = document.querySelector(".container-fluid");
   CONTAINER.innerHTML = "";
-  renderReleasedate(releaseDateRender);  
+  renderReleasedate(releaseDateRender);
 });
 
 const renderReleasedate = (movies) => {
@@ -229,13 +235,13 @@ const renderReleasedate = (movies) => {
     CardDiv.addEventListener("click", () => {
       movieDetails(movie);
     });
-  
+
     CardDiv.appendChild(movieImage);
     CardDiv.appendChild(movieTitle);
     divMovies.appendChild(CardDiv)
     CONTAINER.appendChild(divMovies);
   });
-  
+
 };
 //////////////////////////////////////
 const fetchTopRated= async()=> {
@@ -251,7 +257,7 @@ TopRated.addEventListener("click", async function () {
   const releaseDateRender = await fetchTopRated();
   const CONTAINER = document.querySelector(".container-fluid");
   CONTAINER.innerHTML = "";
-  renderTopRated(releaseDateRender);  
+  renderTopRated(releaseDateRender);
 });
 
 const renderTopRated = (movies) => {
@@ -279,13 +285,13 @@ const renderTopRated = (movies) => {
     CardDiv.addEventListener("click", () => {
       movieDetails(movie);
     });
-  
+
     CardDiv.appendChild(movieImage);
     CardDiv.appendChild(movieTitle);
     divMovies.appendChild(CardDiv)
     CONTAINER.appendChild(divMovies);
   });
- 
+
 };
 ////////////////////////////////
 
@@ -304,7 +310,7 @@ Actors.addEventListener("click", async function () {
   const ActorRender = await fetchActor();
   const CONTAINER = document.querySelector(".container-fluid");
   CONTAINER.innerHTML = "";
-  renderActor(ActorRender);  
+  renderActor(ActorRender);
 });
 
 const renderActor = (movies) => {
@@ -322,11 +328,11 @@ const renderActor = (movies) => {
     const CardDiv = document.createElement("div");
     const movieImage = document.createElement("img");
     const movieTitle = document.createElement("h3");
-    CardDiv.classList.add("card");
+    CardDiv.classList.add("cardAct");
     movieImage.classList.add("card-img-top");
     movieTitle.classList.add("card-title");
     CardDiv.innerHTML = `
-    <img src="${PROFILE_BASE_URL+movie.profile_path}" alt="${
+    <img class= "cardAct" src="${PROFILE_BASE_URL+movie.profile_path}" alt="${
       movie.name
     } poster">
         <h3>${movie.name}</h3>`;
@@ -338,7 +344,7 @@ const renderActor = (movies) => {
     divMovies.appendChild(CardDiv)
     CONTAINER.appendChild(divMovies);
   });
-  
+
 };
 /*static _ReleaseDataConstructUrl() {
   return `${this.TMDB_BASE_URL}/discover/movie?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}&sort_by=release_date.desc`;
@@ -349,7 +355,6 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=23381f9d606c6cff00c6543b3446c4ad&query=" '
 const main = document.getElementById("content");
 const search = document.getElementById("search");
-
 
 const searchconstructUrl = (searchValue) => {
   return `${TMDB_BASE_URL}/search/multi?api_key=${atob('NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI=')}&query=${searchValue}`;
@@ -367,26 +372,24 @@ const renderSearch = async (data) => {
   const resDat = await searchRes(data)
   const searchContent = document.getElementById("content");
 
-    if (resDat === undefined) {
-    alert("undefined")
-  } else if (resDat === movie){
+if (resDat.media_type === movie){
     searchContent.innerHTML = "";
     resDat.results.forEach(element => {
       const elementContainer = document.createElement("div")
       elementContainer.innerHTML=`
       <h3 class="red-text">${element.title}</h3>
-<img width="300" src="${IMG_PATH + element.backdrop_path}">
+<img width="300" class="image" src="${IMG_PATH + element.backdrop_path}">
 `
-elementContainer.classList.add('className')
+//elementContainer.classList.add('className')
   searchContent.appendChild(elementContainer)
 })
-  }else if(resDat === person){
+  }else if(resDat.media_type === person){
     searchContent.innerHTML="";
     resDat.results.forEach(popi => {
       const elementContainer = document.createElement("div")
       elementContainer.innerHTML= `
       <h3 class="red-text">${popi.name}</h3>
-      <img width ="300" src="${PROFILE_BASE_URL + popi.profile_path}">
+      <img width ="300" class="image" src="${PROFILE_BASE_URL + popi.profile_path}">
       `
       searchContent.appendChild(elementContainer)
     })
@@ -402,7 +405,6 @@ form.addEventListener("submit", async (e) => {
   searchIn.innerHTML = "";
   renderSearch(results);
 });
-
 
 
 document.addEventListener("DOMContentLoaded", autorun);
